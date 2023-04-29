2d176afa29fe02d2cc445556a9065d007d4641f5
ichwan1990 <boootx@gmail.com>
2023-04-30 01:13:51 +0700

    master sampeh add form belum bisa input

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UserList from "./components/UserList";
+import AddUser from "./components/AddUser";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<UserList />} />
+          <Route path="add" element={<AddUser />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
